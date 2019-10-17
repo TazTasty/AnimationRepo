a3d8407660ee170e93ae9cb647301222a35b5755
Luke Roby <20.luke.roby@ksd.org>
2019-10-17 10:34:04 -0700

    adding animation project

---
+++ ProgressCheck/Assets/Scripts/EnemyPace.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPace : MonoBehaviour
+{
+    float timer = 0.0f;
+    public Vector2 moveDir;
+    public float moveSpeed = 5.0f;
+    public float paceLength = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //moveDir = new Vector2(1, 0);
+        moveDir.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 3)
+        {
+            //do something
+            moveDir *= -1;
+            timer = 0;
+        }
+        GetComponent<Rigidbody2D>().velocity = moveDir * moveSpeed;
+    }
+}
